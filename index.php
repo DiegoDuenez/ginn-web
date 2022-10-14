78764200bfe54b996544e9d129b4891bbad9aea4
DiegoDuenez <19170154@uttcampus.edu.mx>
2022-10-14 14:45:25 -0500

    add: Add comments in index.php

--- index.php
+++ index.php
@@ -1,30 +1,51 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <!--===== HEADER START =====-->
     <?php include 'templates/header.php'; ?>
+    <!--===== HEADER END =====-->
 </head>
 <body>
     
+    <!--===== MENU START =====-->
     <?php include 'templates/components/menu.php'; ?>
+    <!--===== MENU END =====-->
 
+    <!--===== BANNER START =====-->
     <?php include 'templates/components/banner.php'; ?>
+    <!--===== BANNER END =====-->
 
+    <!--===== NAVBAR START =====-->
     <?php include 'templates/components/navbar.php'; ?>
+    <!--===== NAVBAR END =====-->
 
+    <!--===== PARQUES START =====-->
     <?php include 'templates/layouts/parques.php'; ?>
+    <!--===== PARQUES END =====-->
 
+    <!--===== NOSOTROS START =====-->
     <?php include 'templates/layouts/nosotros.php'; ?>
+    <!--===== NOSOTROS END =====-->
 
+    <!--===== ESPACIOS START =====-->
     <?php include 'templates/layouts/espacios.php'; ?>
+    <!--===== ESPACIOS END =====-->
 
+    <!--===== CLIENTES START =====-->
     <?php include 'templates/layouts/clientes.php'; ?>
+    <!--===== CLIENTES END =====-->
 
+    <!--===== CONTACTO START =====-->
     <?php include 'templates/layouts/contacto.php'; ?>
+    <!--===== CONTACTO END =====-->
 
+    <!--===== FOOTER START =====-->
     <?php include 'templates/components/footer.php'; ?>
+    <!--===== FOOTER END =====-->
 
-
+    <!--===== SCRIPTS START =====-->
     <?php include 'templates/scripts.php'; ?>
+    <!--===== SCRIPTS END =====-->
 
 </body>
 </html>
--- templates/layouts/clientes.php
+++ templates/layouts/clientes.php
@@ -5,42 +5,47 @@
 
     <div class="clientes__container">
 
-            <div class="clientes__image" data-aos='zoom-in'>
-                <img src="resources/img/clientes/coliseo.png" alt="Coliseo" title="Coliseo Centenario">
-            </div>
-            <div class="clientes__image" data-aos='zoom-in'>
-                <img src="resources/img/clientes/fives.png" alt="fives" title="fives">
-            </div>
-            <div class="clientes__image" data-aos='zoom-in'>
-                <img src="resources/img/clientes/gdcopper.png" alt="GDCOPPER" title="GDCOPPER">
-            </div>
-            <div class="clientes__image" data-aos='zoom-in'>
-                <img src="resources/img/clientes/gilbane.png" alt="Gilbane" title="Gilbane">
-            </div>
-            <div class="clientes__image" data-aos='zoom-in'>
-                <img src="resources/img/clientes/hyundai.png" alt="Hyundai" title="Hyundai">
-            </div>
-            <div class="clientes__image" data-aos='zoom-in'>
-                <img src="resources/img/clientes/kone.png" alt="Kone" title="Kone">
-            </div>
-            <div class="clientes__image" data-aos='zoom-in'>
-                <img src="resources/img/clientes/littelfuse.png" alt="Littelfuse" title="Littelfuse">
-            </div>
-            <div class="clientes__image" data-aos='zoom-in'>
-                <img src="resources/img/clientes/tec-milenio.png" alt="Universidad Tecmilenio" title="Universidad Tecmilenio">
-            </div>
-            <div class="clientes__image" data-aos='zoom-in'>
-                <img src="resources/img/clientes/tec-monterrey.png" alt="Tecnológico de Monterrey" title="Tecnológico de Monterrey"> 
-            </div>
-            <div class="clientes__image" data-aos='zoom-in'>
-                <img src="resources/img/clientes/uvm.png" alt="UVM" title="UVM">
-            </div>
-            <div class="clientes__image" data-aos='zoom-in'>
-                <img src="resources/img/clientes/walbridge.png" alt="Walbridge" title="Walbridge">
-            </div>
-            <div class="clientes__image" data-aos='zoom-in'>
-                <img src="resources/img/clientes/yura.png" alt="Yura" title="Yura">
-            </div>
+        <div class="clientes__image" data-aos='zoom-in'>
+            <img src="resources/img/clientes/gilbane.png" alt="Gilbane" title="Gilbane">
+        </div>
+        <div class="clientes__image" data-aos='zoom-in'>
+            <img src="resources/img/clientes/yura.png" alt="Yura" title="Yura">
+        </div>
+        <div class="clientes__image" data-aos='zoom-in'>
+            <img src="resources/img/clientes/fives.png" alt="fives" title="fives">
+        </div>
+        <div class="clientes__image" data-aos='zoom-in'>
+            <img src="resources/img/clientes/hyundai.png" alt="Hyundai" title="Hyundai">
+        </div>
+
+        <div class="clientes__image" data-aos='zoom-in'>
+            <img src="resources/img/clientes/kone.png" alt="Kone" title="Kone">
+        </div>
+        <div class="clientes__image" data-aos='zoom-in'>
+            <img src="resources/img/clientes/littelfuse.png" alt="Littelfuse" title="Littelfuse">
+        </div>
+        <div class="clientes__image" data-aos='zoom-in'>
+            <img src="resources/img/clientes/gdcopper.png" alt="GDCOPPER" title="GDCOPPER">
+        </div>
+        <div class="clientes__image" data-aos='zoom-in'>
+            <img src="resources/img/clientes/walbridge.png" alt="Walbridge" title="Walbridge">
+        </div>
+
+        <div class="clientes__image" data-aos='zoom-in'>
+            <img src="resources/img/clientes/uvm.png" alt="UVM" title="UVM">
+        </div>
+        <div class="clientes__image" data-aos='zoom-in'>
+            <img src="resources/img/clientes/coliseo.png" alt="Coliseo" title="Coliseo Centenario">
+        </div>
+        <div class="clientes__image" data-aos='zoom-in'>
+            <img src="resources/img/clientes/tec-milenio.png" alt="Universidad Tecmilenio" title="Universidad Tecmilenio">
+        </div>
+        <div class="clientes__image" data-aos='zoom-in'>
+            <img src="resources/img/clientes/tec-monterrey.png" alt="Tecnológico de Monterrey" title="Tecnológico de Monterrey"> 
+        </div>
+       
+        
+        
     </div>
    
 
