5fbab8f7c402c1176cb5c33973405d0e66719e49
DiegoDuenez <19170154@uttcampus.edu.mx>
2022-10-14 13:23:51 -0500

    change: Menu

--- templates/scripts.php
+++ templates/scripts.php
@@ -4,12 +4,10 @@
 <script src="libs/sweetalert2/sweetalert2.all.min.js"></script>     
 <script src="libs/menu/js/menu.min.js"></script>
 <script src="js/banner.js"></script>     
-<script src="js/index.js"></script>     
-
-
 <script>
     Menu = new Menu({options: {element: '.menu', openWith: '.navbar__button', closeWith: '.menu__button', size:'20rem', from: 'right'}})
     Menu.init();
-    console.log(Menu.isOpen)
+    Menu.closeWith('.menu__link')
     AOS.init();
 </script>
+<script src="js/index.js"></script>     
