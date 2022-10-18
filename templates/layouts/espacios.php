2a4d028baf40ebd29f0fb0c8359a4d483ec00294
DiegoDuenez <19170154@uttcampus.edu.mx>
2022-10-18 14:33:35 -0500

    change:

--- templates/layouts/espacios.php
+++ templates/layouts/espacios.php
@@ -1,12 +1,6 @@
 <section class="espacios" id="espaciosdisponibles">
     <h2 class="title title--dark">Espacios disponibles</h2>
     <div class="espacios__estados">
-        <!--<div class="espacios__estado espacios__estado--active">Coahuila</div>
-        <div class="espacios__estado">Durango</div>
-        <div class="espacios__estado">Guerrero</div>
-        <div class="espacios__estado">Nayarit</div>
-        <div class="espacios__estado">Sinaloa</div>-->
-
         <?php 
         require_once("admin/bd/Ciudades/Ciudades.php");
         $Ciudades = new Ciudades();
@@ -24,18 +18,6 @@
         ?>
     </div>
     <div class="espacios__container" id="espaciosContainer">
-        <!-- <div class="espacios__card" data-aos="fade-up">
-            <div class="espacios__card-header">
-                <img src="resources/img/mision-img.png" alt="Parque">
-            </div>  
-            <div class="espacios__card-body">
-                <p>Nave industrial oriente</p>
-                <p class="espacios__card-title">Cedis III</p>
-                <p><i class="fa-solid fa-location-dot"></i> Parque industrial oriente Torreón Coahuila</p>
-                <p><img src="resources/img/terreno.png" width="20px"> 2,500 m2</p>
-                <p><img src="resources/img/terreno.png" width="20px"> 1,000 m2 de oficinas</p>
-            </div>  
-        </div> -->
-        
+      
     </div>
 </section>
